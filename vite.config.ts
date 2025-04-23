4b84d12f5d1600cf39552992865562cc7e6cb1ba
Diana-Khalina <diaaana@i.ua>
2025-04-22 22:35:19 -0400

    Set port for Render

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000, // ✅ Explicitly setting the port
+    host: "0.0.0.0", // ✅ Allow Render to expose the network
+  },
   envDir: './',
   plugins: [react()],
 });
