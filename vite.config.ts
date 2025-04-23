00a1b01bd17c40b08d0b3c42d22b3d0b5c73f8cd
Diana-Khalina <diaaana@i.ua>
2025-04-22 22:37:56 -0400

    fixed config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000, // ✅ Explicitly setting the port
-    host: "0.0.0.0", // ✅ Allow Render to expose the network
+    port: 3000, 
+    host: "0.0.0.0", 
   },
   envDir: './',
   plugins: [react()],
